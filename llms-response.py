b7d1d788aed9387dc3ea7ae4d00ce0d9c6e472f3
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-18 11:33:36 -0400

    got ollama working

--- llms-response.py
+++ llms-response.py
@@ -7,9 +7,12 @@ client = Client(
   host='http://localhost:11434',
   headers={'x-some-header': 'some-value'}
 )
-response = client.chat(model='deepseek-rq', messages=[
+response = client.chat(model='deepseek-r1', messages=[
   {
     'role': 'user',
     'content': 'Why is the sky blue?',
   },
 ])
+
+
+print(response)
