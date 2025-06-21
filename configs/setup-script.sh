69d42d8a1489d46181300a5f7d836cc7f9e6f32e
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-21 14:54:30 -0400

    cant automate so removed unwanted things

--- configs/setup-script.sh
+++ configs/setup-script.sh
@@ -4,7 +4,7 @@ curl -L https://github.com/prometheus/prometheus/releases/download/v3.4.1/promet
 
 tar xvfz prometheus-*.tar.gz
 
-# cd prometheus-*
+rm prometheus-*.tar.gz
 
 #downloading grafana
 
@@ -40,80 +40,5 @@ curl -L https://github.com/prometheus/node_exporter/releases/download/v1.9.1/nod
 
 tar xvfz node_exporter-*.*-amd64.tar.gz
 
-
-# editing the prometehus config file
-
-YML_FILE = '/root/prometheus/prometheus.yml'
-
-cat <<EOF > "$YML_FILE"
-# my global config
-global:
-    scrape_interval: 15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
-  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
-  # scrape_timeout is set to the global default (10s).
-
-# Alertmanager configuration
-alerting:
-  alertmanagers:
-    - static_configs:
-        - targets:
-          # - alertmanager:9093
-rule_files:
-  # - "first_rules.yml"
-  # - "second_rules.yml"
-
-# A scrape configuration containing exactly one endpoint to scrape:
-# Here it's Prometheus itself.
-scrape_configs:
-  
-  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
-
-  # run prometheus
-  - job_name: "prometheus"
-
-    # prometheus port
-    static_configs:
-      - targets: ["0.0.0.0:9090"]
-          app: "prometheus"
-  
-  # node exporter
-  - job_name: node
-
-    # node exporter port
-    static_configs:
-      - targets: ['localhost:9100']
-
-  # running nvidia-gpu-exporter 
-  - job_name: 'nvidia_gpu'
-    
-    # gpu exporter port
-    static_configs:
-      - targets: ['localhost:9835']
-
-EOF
-
-#creating prometheus as a service
-
-cd ..
-
-
-SERVICE_NAME = "prometheus.service"
-SERVICE_PATH = "etc/systemd/system/$SERVICE_NAME"
-
-sudo tee "$SERVICE_PATH" > /dev/null <<EOF 
-[Unit]
-Description=ROT13 demo service
-After=network.target
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=1
-User=centos
-ExecStart=/root/prometheus
-
-[Install]
-WantedBy=multi-user.target
-
+rm node_exporter-*.*-amd64.tar.gz
 
