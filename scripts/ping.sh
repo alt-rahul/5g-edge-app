349f94b7a2fb51373ab200942922ee41cc2f3327
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-24 19:41:30 -0400

    attaining saved files

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -7,6 +7,10 @@ echo q | htop | aha --black --line-fix > htop.html
 for ((i = 0 ; i < 300 ; i++ )); do echo q | htop | aha --black --line-fix >> htop.html; sleep 1s; done
 
 
+# to bring these file back locally:
+
+scp root@srv1-lg1:/root/htop.html . # in console
+scp rahulrajkumar@console.sb2.cosmos-lab.org:htop.html .
 
 
 
