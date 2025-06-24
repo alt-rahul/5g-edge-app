e928ddcfc35af04c9379af913638900c335cc1a7
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-24 19:39:24 -0400

    collect htop data

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,3 +1,12 @@
-nvidia > result.txt
+nvidia-smi > result.txt
 
-for ((i = 0 ; i < 10 ; i++ )); do nvidia-smi >> result.txt; sleep 1s; done
+for ((i = 0 ; i < 300 ; i++ )); do nvidia-smi >> result.txt; sleep 1s; done
+
+echo q | htop | aha --black --line-fix > htop.html
+
+for ((i = 0 ; i < 300 ; i++ )); do echo q | htop | aha --black --line-fix >> htop.html; sleep 1s; done
+
+
+
+
+
