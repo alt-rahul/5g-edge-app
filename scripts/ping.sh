e46939f89a21a1e260462b9d975c1dc64c33b230
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-25 15:13:28 -0400

    save top results as a csv

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -6,11 +6,17 @@ echo q | htop | aha --black --line-fix > htop.html
 
 for ((i = 0 ; i < 300 ; i++ )); do echo q | htop | aha --black --line-fix >> htop.html; sleep 1s; done
 
+# to save it as a csv file
+for ((i = 0 ; i < 300 ; i++ )); do ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | awk 'BEGIN {OFS=","} NR==1 {print "PID,PPID,CMD,%MEM,%CPU"} NR>1 {print $1,$2,$3,$4,$5}' > processes.csv; sleep 1s; done
 
 # to bring these file back locally:
 
 scp root@srv1-lg1:/root/htop.html . # in console
 scp rahulrajkumar@console.sb2.cosmos-lab.org:htop.html .
 
+
+# for stressing the compute with artifical load using stres-ng
+
+stress-ng -c 0 -l 40
 
 
