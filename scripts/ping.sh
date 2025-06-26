200e54cb633c6d1bb5e38e84bd473fd4fc7cb994
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-25 21:32:18 -0400

    omg taking this long for reason bro...why I am like this fr fr. it was lowk just a single command but i think there is no gpu utilization from the exporters so what are we even doing here?

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -7,7 +7,7 @@ echo q | htop | aha --black --line-fix > htop.html
 for ((i = 0 ; i < 300 ; i++ )); do echo q | htop | aha --black --line-fix >> htop.html; sleep 1s; done
 
 # to save it as a csv file
-for ((i = 0 ; i < 300 ; i++ )); do ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | awk 'BEGIN {OFS=","} NR==1 {print "PID,PPID,CMD,%MEM,%CPU"} NR>1 {print $1,$2,$3,$4,$5}' > processes.csv; sleep 1s; done
+for ((i = 0 ; i < 10 ; i++ )); do (ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | awk 'BEGIN {OFS=","} NR==1 {print "PID,PPID,CMD,%MEM,%CPU"} NR>1 {print $1,$2,$3,$4,$5}' > processes.csv); sleep 1s; done
 
 # to bring these file back locally:
 
