eee9d1b4a9b91f3c54077843184c4d283b21d506
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-28 13:27:41 -0400

    installing cuda toolkit instructions

--- scripts/nvbench.sh
+++ scripts/nvbench.sh
@@ -27,7 +27,16 @@ sudo rm -rf /usr/local/bin/cmake /usr/local/bin/ctest /usr/local/bin/cpack /usr/
 sudo rm -rf /usr/local/share/cmake-*
 sudo rm -rf /usr/local/lib/cmake
 
-    #redo lines 9-23 above, check version if it doesn't work do the following:
-    echo $PATH
-    export PATH=/usr/local/bin:$PATH
-    source ~/.bashrc
+#redo lines 9-23 above, check version if it doesn't work do the following:
+echo $PATH
+export PATH=/usr/local/bin:$PATH
+source ~/.bashrc
+
+#to install cuda toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-ubuntu2204-12-9-local_12.9.1-575.57.08-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-9-local_12.9.1-575.57.08-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-9
