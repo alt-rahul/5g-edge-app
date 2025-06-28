4cce99103fe24e3516962d36caa1245bee981c39
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-28 15:54:23 -0400

    update order of installing cmake

--- scripts/nvbench.sh
+++ scripts/nvbench.sh
@@ -4,20 +4,17 @@ cd nvbench_demo
 
 cmake -DCMAKE_CUDA_ARCHITECTURES=native .
 
+sudo apt update
+sudo apt install libssl-dev
+sudo apt install cmake
+
+
 #had an error with cmake (now I need to install)
 
 wget https://github.com/Kitware/CMake/releases/download/v4.1.0-rc1/cmake-4.1.0-rc1.tar.gz
 tar -xf cmake-4.1.0-rc1.tar.gz
 cd cmake-4.1.0-rc1
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
-
-#if you get an openSSL error then do the following:
-sudo apt update
-sudo apt install libssl-dev
-
-#then continue wit the followign
-cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
 make -j$(nproc)
 sudo make install
