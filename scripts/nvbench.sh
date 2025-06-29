7f0630ba8ee3b3f297deac7ddf736700415c6e8e
rahul raj <rahul.rajkumar@rutgers.edu>
2025-06-28 22:06:41 -0400

    add proper roadmap for installing nvbench

--- scripts/nvbench.sh
+++ scripts/nvbench.sh
@@ -1,63 +1,53 @@
-git clone https://github.com/NVIDIA/nvbench_demo.git
-
-cd nvbench_demo
-
-cmake -DCMAKE_CUDA_ARCHITECTURES=native .
-
 
 # 1. first install cmake and it dependencies
-sudo apt update
-sudo apt install libssl-dev
-sudo apt install cmake
+    sudo apt update
+    sudo apt install libssl-dev
+    sudo apt install cmake
 
+# 2. install most updated version of cmake
+    wget https://github.com/Kitware/CMake/releases/download/v4.1.0-rc1/cmake-4.1.0-rc1.tar.gz
+    tar -xf cmake-4.1.0-rc1.tar.gz
+    cd cmake-4.1.0-rc1
+    mkdir build && cd build
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+    make -j$(nproc)
+    sudo make install
+    #check if cmake version is the right version
+    cmake --version # if it is showing an older version, continue to do the following
+    #change path location
+    echo $PATH
+    export PATH=/usr/local/bin:$PATH
+    source ~/.bashrc
+    #check again - should work (worked for me atleast)
+    cmake --version
 
-#had an error with cmake (now I need to install)
+# 3. Now install nvcc 
+    apt install nvidia-cuda-toolkit
+    #check version
+    nvcc --version
+    #to install updated cuda toolkit (make sure to download the right installer, go this website: https://developer.nvidia.com/cuda-downloads)
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+    sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
+    sudo cp /var/cuda-repo-ubuntu2004-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
+    sudo apt-get update
+    sudo apt-get -y install cuda-toolkit-12-9
+    #check nvcc version
+    nvcc --version
+    #change path location
+    export PATH=/usr/local/cuda-12.9/bin:$PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-12.9/lib64:$LD_LIBRARY_PATH
+    #set pointers to new cuda version
+    sudo update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.9 1
+    sudo update-alternatives --set cuda /usr/local/cuda-12.9
+    sudo update-alternatives --display cuda
+    #check pointers
+    ls -l /usr/local/cuda
 
-wget https://github.com/Kitware/CMake/releases/download/v4.1.0-rc1/cmake-4.1.0-rc1.tar.gz
-tar -xf cmake-4.1.0-rc1.tar.gz
-cd cmake-4.1.0-rc1
-mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
-make -j$(nproc)
-sudo make install
-
-#check if cmake version is the right version
-cmake --version # if it is showing an older version, continue to do the following
-
-#this may not work, but there is a fix to this:
-sudo rm -rf /usr/local/bin/cmake /usr/local/bin/ctest /usr/local/bin/cpack /usr/local/bin/cmake-gui
-sudo rm -rf /usr/local/share/cmake-*
-sudo rm -rf /usr/local/lib/cmake
-
-#redo lines 9-23 above, check version if it doesn't work do the following:
-echo $PATH
-export PATH=/usr/local/bin:$PATH
-source ~/.bashrc
-
-#to install cuda toolkit (make sure to download the right installer, go this website: https://developer.nvidia.com/cuda-downloads)
-wwget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-9
-
-export PATH=/usr/local/cuda-12.9/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-12.9/lib64:$LD_LIBRARY_PATH
-
-export CUDACXX=/usr/local/cuda-12.9/bin/nvcc
-cmake .. -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.9/bin/nvcc
-make -j
-
-ls -l /usr/local/cuda
-
-
-# 2. installed cmake new version
-# 3. set path using echo path
-# 4. install nvcc using apt
-# 5. upgrade nvcc version using cuda toolkit online
-# 6. set new nvcc version path
-# 7. delete cmake build dir and make a new dir
-# 8. run cmake with new nvcc version
-# 9. 
+#4. Now inistall nvbench
+    git clone https://github.com/NVIDIA/nvbench_demo.git
+    cd nvbench_demo
+    cmake -DCMAKE_CUDA_ARCHITECTURES=native .
+    make
+    ./example_bench
